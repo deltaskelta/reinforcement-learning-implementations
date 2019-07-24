9844c6e029f36a019ae6b40264d00e947868f41a
Jeff Willette <jrwillette88@gmail.com>
2019-07-25 00:39:29 +0900

    added animated gifs for figs 3.2 and 3.5

--- 03-MDP/grid_world.py
+++ 03-MDP/grid_world.py
@@ -4,54 +4,18 @@ import random
 from typing import List, Tuple
 
 import numpy as np
-from matplotlib import animation, rc, pyplot as plt
-
-rc('animation', html='html5')
+from matplotlib import animation, pyplot as plt
 
 DIMS = 5
-ACTIONS = [(0, -1), (-1, 0), (1, 0), (0, 1)]  # action tuples
-PROB = 0.25
-DISCOUNT = 0.9
 EPSILON = 1e-1
-
-# special position tuples
+ACTIONS = [(0, -1), (-1, 0), (1, 0), (0, 1)]
+DISCOUNT = 0.9
 A = (0, 1)
 B = (0, 3)
 A_PRIME = (4, 1)
 B_PRIME = (2, 3)
 
-#def plot(grid: [np.ndarray], diff: int):
-#    """plot the current grid and the differences between iterations"""
-#    text = []
-#    for v in grid:
-#        text.append(['{:1.1f}'.format(n) for n in v])
-#
-#    _, axs = plt.subplots(2, 1)
-#    axs[0].axis('off')
-#
-#    labels = [i + 1 for i in range(DIMS)]
-#    axs[0].table(cellText=text, colLabels=labels, rowLabels=labels, loc='center')
-#    axs[1].set_xlabel('v_pi iteration differences')
-#    axs[1].plot(diff)
-#
-#    plt.show()
-
-fig, axs = plt.subplots(2, 1)
-
-
-def plot(grid: np.ndarray, diff: List[int]) -> 'matplotlib.image.AxesImage':
-    """makes an image of the current state"""
-
-    text = list()
-    for v in grid:
-        text.append([['{:1.1f}'.format(i) for i in v]])
-
-    axs[0].axis('off')
-    axs[0].table(cellText=text, loc='center')
-    axs[1].set_xlabel('v_pi iteration differences')
-    axs[1].plot(diff)
-
-    return plt.imshow(axs)
+fig, [tbl_axs, line_axs] = plt.subplots(2, 1)
 
 
 def step(state: Tuple[int, int], action: Tuple[int, int]) -> (Tuple[int, int], int):
@@ -71,41 +35,70 @@ def step(state: Tuple[int, int], action: Tuple[int, int]) -> (Tuple[int, int], i
     return new_state, 0
 
 
-def fig_3_2() -> 'matplotlib.image.AxesImage':
-    """implementation of the state value function (v_pi(s)) in figure 3.2 of the book. Same probability for each action"""
+def plot_init() -> List[plt.Artist]:
+    """make a blank plot for a new animation"""
+    tbl_axs.clear()
+    tbl_axs.axis('off')
+    tbl_axs.table(cellText=[['0' for i in range(DIMS)] for i in range(DIMS)], loc='center')
 
-    grid = np.zeros((DIMS, DIMS))
-    images = list()
-    diffs = list()
-    while True:
+    line_axs.clear()
+    line_axs.set_xlabel('v_pi iteration differences')
+    line_axs.plot([], [], 'b', animated=True)
 
-        new_grid = np.zeros(grid.shape)  # all updates happen simultaneously
+    return tbl_axs, line_axs
+
+
+class GridWorld():
+    """implementation of figures 3.2 and 3.6 gridworld with equiprobable policy"""
+
+    def __init__(self):
+        self.cont = True
+        self.grid = np.zeros((DIMS, DIMS))
+        self.diffs = []
+        self.line_x = []
+
+    def plot(self) -> None:
+        """plot the updates"""
+
+        text = list()
+        for v in self.grid:
+            text.append(['{:1.1f}'.format(i) for i in v])
+
+        tbl_axs.table(cellText=text, loc='center')
+        line_axs.plot(self.line_x, self.diffs, 'b')
+
+    def loop_until_convergence(self) -> int:
+        """loops over the policy until convergence is reached"""
+        i = 0
+        while self.cont:
+            i += 1
+            yield i
+
+    def iterate_3_2(self, f) -> List[plt.Artist]:
+        """iterate to improve the policy value with equiprobable policy"""
+
+        new_grid = np.zeros(self.grid.shape)  # all updates happen simultaneously
         for i in range(DIMS):
             for j in range(DIMS):
                 for action in ACTIONS:
                     (next_i, next_j), reward = step((i, j), action)
-                    new_grid[i, j] += 0.25 * (reward + DISCOUNT * grid[next_i, next_j])
+                    new_grid[i, j] += 0.25 * (reward + DISCOUNT * self.grid[next_i, next_j])
 
-        diffs.append(np.sum(np.abs(grid - new_grid)))
-        images.append(plot(new_grid, diffs))
+        self.diffs.append(np.sum(np.abs(self.grid - new_grid)))
+        self.line_x.append(f)
+        print(self.diffs, self.line_x)
+        self.plot()
 
-        if diffs[-1] < EPSILON:
-            print("breaking")
-            print(grid, new_grid, diffs)
-            break
+        if self.diffs[-1] < EPSILON:
+            self.cont = False
 
-        grid = new_grid
-    return images
+        self.grid = new_grid
+        return tbl_axs, line_axs
 
+    def iterate_3_5(self, f) -> List[plt.Artist]:
+        """iterate to improve the polocy value with greedy policy"""
 
-def fig_3_5() -> 'matplotlib.image.AxesImage':
-    """implementation of the state value function (v_pi(s)) in figure 3.2 of the book. Always takes greedy action"""
-
-    grid = np.zeros((DIMS, DIMS))
-    diffs = list()
-    while True:
-
-        new_grid = np.zeros(grid.shape)  # all updates happen simultaneously
+        new_grid = np.zeros(self.grid.shape)  # all updates happen simultaneously
         for i in range(DIMS):
             for j in range(DIMS):
                 best = float("-inf")
@@ -113,25 +106,29 @@ def fig_3_5() -> 'matplotlib.image.AxesImage':
 
                 for action in ACTIONS:  # find the greedy action
                     (x, y), reward = step((i, j), action)
-                    value = reward + DISCOUNT * grid[x, y]
+                    value = reward + DISCOUNT * self.grid[x, y]
                     if value > best:
                         best = value
 
                 new_grid[i, j] += best
 
-        diffs.append(np.sum(np.abs(grid - new_grid)))
-        if diffs[-1] < EPSILON:
-            break
+        self.diffs.append(np.sum(np.abs(self.grid - new_grid)))
+        self.line_x.append(f)
+        self.plot()
 
-        grid = new_grid
+        if self.diffs[-1] < EPSILON:
+            self.cont = False
+
+        self.grid = new_grid
+        return tbl_axs, line_axs
 
 
 if __name__ == "__main__":
 
-    imgs = fig_3_2()
-    print("after return")
-    anim = animation.ArtistAnimation(fig, imgs, blit=True)
-    plt.show()
-    anim.save('../images/grid_world_3_5.gif', writer='imagemagick', fps=15)
+    gw = GridWorld()
+    anim = animation.FuncAnimation(fig, gw.iterate_3_2, frames=gw.loop_until_convergence, init_func=plot_init)
+    anim.save('grid_world_3_2.gif', writer='imagemagick')
 
-    #fig_3_5()
+    gw = GridWorld()
+    anim = animation.FuncAnimation(fig, gw.iterate_3_5, frames=gw.loop_until_convergence, init_func=plot_init)
+    anim.save('grid_world_3_5.gif', writer='imagemagick')
